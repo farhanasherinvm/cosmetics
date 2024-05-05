ce3ec3ad42ce3e25d3a2e2b00b71a3ae44b9b93d
farhansherinvm <farhanasherinvm@gmail.com>
2024-05-06 01:56:26 +0530

    payment model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from accounts.models import User_Profile
 # Create your models here.
+
+class Payment(models.Model):
+    user      = models.ForeignKey(User_Profile, on_delete=models.CASCADE)
+    payment_id= models.CharField(max_length=100)
+    method    = models.CharField(max_length=100)
+    amount    = models.CharField(max_length=100)
+    status    = models.CharField(max_length=100)
+    created_at= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.method
