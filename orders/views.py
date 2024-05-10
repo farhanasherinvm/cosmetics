30f04b3d8e1e8b860aa79c1d7bd10c65dc27834d
farhansherinvm <farhanasherinvm@gmail.com>
2024-05-10 23:48:37 +0530

    Cod just function

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
@@ -6,3 +6,5 @@ def order_success(request):
     return render (request ,"order_success.html")
 def order_tracking(request):
     return render(request, "order_tracking.html")
+def cash_on_delivery(request):
+    return redirect ('outgoing:checkout')
