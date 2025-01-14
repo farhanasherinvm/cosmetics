4ed401009e7626b887f69df962e9e6fd8c65a1a8
farhansherinvm <farhanasherinvm@gmail.com>
2025-01-15 01:42:45 +0530

    sideimg

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from.models import Product, ProductVariant
+from.models import Product, ProductVariant,SideImage
 # Register your models here.
 
-admin.site.register(Product)
+
+class SideImageInline(admin.TabularInline):
+    model = SideImage
+    extra = 1  # Number of empty side image forms to display in the admin
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [SideImageInline]
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(SideImage)
 admin.site.register(ProductVariant)
--- products/models.py
+++ products/models.py
@@ -19,11 +19,18 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
-    
+
+
+class SideImage(models.Model):
+    product = models.ForeignKey(Product, related_name='side_images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='photos/side_images' ,blank=True, null=True)  # Additional image for the variant
+
+    def __str__(self):
+        return self.product.product_name
     
 class ProductVariant(models.Model):
     product = models.ForeignKey(Product,related_name='variants',on_delete=models.CASCADE)
-    image   = models.ImageField(upload_to='variants/' , blank=True, null=True)
+    image   = models.ImageField(upload_to='photos/variants' , blank=True, null=True)
     color   = models.CharField(max_length=100)
     price   = models.DecimalField(max_digits=10,decimal_places=2,blank=True,null=True)
     stock   = models.PositiveIntegerField(default=0)
